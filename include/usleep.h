38ef2077ed1da9cd182b3e33e622099d2adc1571
Josh Watts <jwatts@d3engineering.com>
2013-03-27 17:42:34 -0400

    Implement usleep using clkdelay

---
+++ include/usleep.h
@@ -0,0 +1,11 @@
+/**
+ * @file usleep.h
+ */
+/* Embedded Xinu, Copyright (C) 2009.  All rights reserved. */
+
+#ifndef _USLEEP_H_
+#define _USLEEP_H_
+
+void usleep(ulong usec);
+
+#endif                          /* _USLEEP_H_ */
---
+++ system/usleep.c
@@ -0,0 +1,27 @@
+/**
+ * @file     usleep.c
+ * @provides usleep.
+ */
+/* Embedded Xinu, Copyright (C) 2009.  All rights reserved. */
+
+#include <kernel.h>
+#include <stddef.h>
+#include <platform.h>
+#include <interrupt.h>
+#include <clock.h>
+#include <queue.h>
+#include "conf.h"
+
+extern void clkdelay(ulong cycles);
+
+#if RTCLOCK
+
+/**
+ * Sleep for N microseconds
+ */
+void usleep(ulong usec)
+{
+    clkdelay((usec * platform.clkfreq) / CLKTICKS_PER_SEC);
+}
+
+#endif                          /* RTCLOCK */
